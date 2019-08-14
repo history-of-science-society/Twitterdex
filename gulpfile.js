0b3775edf71fd46da65f03de9a35163007245a4e
royalfig <ryan.feigenbaum@gmail.com>
2019-08-14 16:42:43 -0400

    dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,36 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const minify = require('gulp-minify');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
 
 gulp.task('sass', function () {
-    return gulp.src('./src/scss/**/*.scss')
+    return gulp.src('src/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest('src/css'));
 });
 
-gulp.task('compress', function() {
+gulp.task('compress', function () {
     gulp.src('src/js/*.js')
-      .pipe(minify())
-      .pipe(gulp.dest('dist/js'))
-  });
+        .pipe(minify())
+        .pipe(gulp.dest('dist/js'))
+});
 
+gulp.task('autoprefix', () =>
+    gulp.src('src/css/main.css')
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('dist/css'))
+);
+
+gulp.task('minify-css', () => {
+    return gulp.src('dist/css/*.css')
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(gulp.dest('dist/css/min'));
+  });
 
 gulp.task('watch', function () {
     gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
