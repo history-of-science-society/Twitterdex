138d29a1d682a0b8b2abb20a065d856d2b21739e
royalfig <ryan.feigenbaum@gmail.com>
2019-08-15 10:29:38 -0400

    gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -7,35 +7,37 @@ const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const babel = require('gulp-babel');
 
-
+// CSS tasks
 gulp.task('sass', function () {
     return gulp.src('src/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('src/css'));
 });
 
-
 gulp.task('autoprefix', () =>
-gulp.src('src/css/main.css')
-.pipe(autoprefixer({
-    browsers: ['last 2 versions'],
-    cascade: false
-}))
-.pipe(gulp.dest('dist/css'))
+    gulp.src('src/css/main.css')
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('dist/css'))
 );
 
 gulp.task('minify-css', () => {
     return gulp.src('dist/css/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist/css/min'));
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest('dist/css/min'));
 });
 
+// JS tasks
 gulp.task('babel', () =>
-gulp.src('src/js/main.js')
-.pipe(babel({
-    presets: ['@babel/env']
-}))
-.pipe(gulp.dest('dist/js'))
+    gulp.src('src/js/main.js')
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    .pipe(gulp.dest('dist/js'))
 );
 
 gulp.task('compress', function () {
@@ -44,6 +46,29 @@ gulp.task('compress', function () {
         .pipe(gulp.dest('dist/js/min'))
 });
 
+// Watch
 gulp.task('watch', function () {
     gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
 });
+
+
+// CSS Runner
+gulp.task('css', function () {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest('dist/css'));
+})
+
+// JS Runner
+gulp.task('js', function () {
+    return gulp.src('src/js/*.js')
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    .pipe(minify())
+    .pipe(gulp.dest('dist/js'))
+})
