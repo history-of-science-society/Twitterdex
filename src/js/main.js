fbff21cb3ad6de566d8aea16636cec070ea14172
Ryan Feigenbaum <rfeigenb@ND.EDU>
2020-07-22 13:21:39 -0400

    added animations!

--- src/js/main.js
+++ src/js/main.js
@@ -136,32 +136,37 @@ ul.addEventListener("click", (e) => {
   btn.innerHTML =
     '<svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-label="Close" stroke="#ffffff" stroke-width="2" stroke-linecap="square" stroke-linejoin="miter" fill="none" color="#ffffff"> <title id="cancelIconTitle">Cancel</title> <path d="M15.5355339 15.5355339L8.46446609 8.46446609M15.5355339 8.46446609L8.46446609 15.5355339"/> <path d="M4.92893219,19.0710678 C1.02368927,15.1658249 1.02368927,8.83417511 4.92893219,4.92893219 C8.83417511,1.02368927 15.1658249,1.02368927 19.0710678,4.92893219 C22.9763107,8.83417511 22.9763107,15.1658249 19.0710678,19.0710678 C15.1658249,22.9763107 8.83417511,22.9763107 4.92893219,19.0710678 Z"/> </svg>';
   btn.classList.add("close-modal-btn");
+  div.classList.add("t-modal");
+  div.innerHTML = modal;
+  div.append(btn);
+  document.body.append(div);
 
-  btn.addEventListener("click", (e) => {
-    const modalToClose = e.currentTarget.parentElement;
-    modalToClose.parentNode.removeChild(modalToClose);
-    div = null;
-  });
+  // closing animation
+  const closingAnimation = () => {
+    div.firstElementChild.addEventListener("animationend", () => {
+      div.parentNode.removeChild(div);
+      div = null;
+    });
+    div.firstElementChild.classList.add("leave");
+    div.classList.add("fade-out");
+  };
 
+  // Add event listener to close button
+  btn.addEventListener("click", closingAnimation);
+
+  // Add event listener to container
   div.addEventListener("click", (e) => {
     if (e.target === e.currentTarget) {
-      e.currentTarget.parentNode.removeChild(e.currentTarget);
-      div = null;
+      closingAnimation();
     }
   });
 
   const closeByEsc = (e) => {
     if (e.key === "Escape" || e.code === "Escape") {
-      div.parentNode.removeChild(div);
-      div = null;
+      closingAnimation();
       document.removeEventListener("keyup", closeByEsc);
     }
   };
 
   document.addEventListener("keyup", closeByEsc);
-
-  div.classList.add("t-modal");
-  div.innerHTML = modal;
-  div.append(btn);
-  document.body.append(div);
 });
